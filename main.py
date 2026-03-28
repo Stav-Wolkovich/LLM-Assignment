c3d4fb58a5392f40bc6687629f7954ff30cb3ea9
lidoratias <lidor1atias@gmail.com>
2026-03-28 15:52:26 +0300

    Phases 1-2 now complete. Both chat-mode and agentic mode work great. There's also a quota check that warns the user if he get's too close to his monthly/daily/hourly limit.

--- main.py
+++ main.py
@@ -7,23 +7,43 @@ class Mode(enum.Enum):
     CHAT = 0
     AGENTIC = 1
 
-def get_payload(mode, chat_history, reasoning):
+def get_chat_mode_payload(chat_history):
+    return {
+        "messages": chat_history,
+        "model": "gpt-5-nano",
+        "max_completion_tokens": 2000
+    }
+
+def get_agent_mode_payload(user_input, reasoning, previous_response_id):
+    return {
+        "input": user_input,
+        "model": "gpt-5-nano",
+        "max_completion_tokens": 2000,
+        "tools": [
+            {"type": "web_search"}
+        ],
+        "reasoning": reasoning,
+        "previous_response_id": previous_response_id
+    }
+
+def get_api_url(mode):
     if mode == Mode.CHAT:
-        return {
-            "messages": chat_history,
-            "model": "gpt-5-nano",
-            "max_completion_tokens": 2000
-        }
+        return "https://server.iac.ac.il/api/v1/studentapi/chat/completions"
     elif mode == Mode.AGENTIC:
-        return {
-            "messages": chat_history,
-            "model": "gpt-5-nano",
-            "max_completion_tokens": 2000,
-            "tools": [
-                {"type": "web_search"}
-            ],
-            "reasoning": reasoning
-        }
+        return "https://server.iac.ac.il/api/v1/studentapi/responses"
+    else:
+        raise ValueError("Invalid mode")
+    
+
+def get_response(mode, data):
+    if mode == Mode.CHAT:
+        return data.get("choices", [{}])[0].get("message", {}).get("content")
+    elif mode == Mode.AGENTIC:
+        message = next(item for item in data['output'] if item['type'] == 'message')
+        return message['content'][0]['text']
+    else:
+        raise ValueError("Invalid mode")
+
 
 def handle_non_query_inputs(user_input, mode, reasoning):
         if user_input in ["exit", "quit"]:
@@ -41,6 +61,18 @@ def handle_non_query_inputs(user_input, mode, reasoning):
             return "continue", mode, reasoning
         else:
             return "none", mode, reasoning
+        
+
+def check_limit(quota, limit_key, used_key, label):
+    limit_val = quota.get(limit_key, 0)
+    used_val = quota.get(used_key, 0)
+
+    remaining = limit_val - used_val
+
+    if remaining <= 2000:
+        print(f"⚠️ Warning: {label} quota is almost exhausted!")
+        print(f"   Used: {used_val} / {limit_val} (Remaining: {remaining})\n")
+
 
 def main():
     print("Welcome to my Web Chat, based on ChatGPT-NANO-5")
@@ -54,6 +86,7 @@ def main():
     mode = Mode.CHAT
     chat_history = []
     reasoning = {"effort": "low"}
+    previous_response_id = None
 
     while True:
         user_input = input("You: ")
@@ -63,12 +96,13 @@ def main():
         elif action == "continue":
             continue
 
-        chat_history.append({"role": "user", "content": user_input})
+        if mode == Mode.CHAT:
+            chat_history.append({"role": "user", "content": user_input})
 
-        payload=get_payload(mode, chat_history, reasoning)
+        payload = get_chat_mode_payload(chat_history) if mode == Mode.CHAT else get_agent_mode_payload(user_input, reasoning, previous_response_id)        
         
         response = requests.post(
-            "https://server.iac.ac.il/api/v1/studentapi/chat/completions",
+            get_api_url(mode),
             headers={
                 "Authorization": f"Bearer {TOKEN}",
                 "Content-Type": "application/json"
@@ -78,11 +112,21 @@ def main():
         
         if response.status_code == 200:
             data = response.json()
-            message = data['choices'][0]['message']
-            bot_reply = message.get('content') or message.get('reasoning') or ''
-            chat_history.append({"role": "assistant", "content": bot_reply})
-            print(f"ChatGPT-NANO-5: {bot_reply}")
+            resp = get_response(mode, data)
+            current_id = data.get('id')
 
+            if (mode == Mode.AGENTIC):
+                previous_response_id = current_id
+            elif mode == Mode.CHAT:
+                chat_history.append({"role": "assistant", "content": resp})
+
+            print(f"ChatGPT-NANO-5: {resp}")
+
+            quota = data.get("iac_quota_status", {})
+            # Check all quotas
+            check_limit(quota, "limit_daily", "tokens_used_daily", "Daily")
+            check_limit(quota, "limit_hourly", "tokens_used_hourly", "Hourly")
+            check_limit(quota, "limit_monthly", "tokens_used_monthly", "Monthly")
         else:
             print(f"Error: {response.status_code} - {response.text}")
 
